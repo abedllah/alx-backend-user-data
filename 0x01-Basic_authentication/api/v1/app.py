d4f9f745fa1ff99315eefd6509dde985539ab45b
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2024-09-19 01:31:50 +0100

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+if getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+elif getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
